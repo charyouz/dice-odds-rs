86e873e4422a7df5f3321a3165f9ebfd99abd7fe
Jouni Säilä <jouni.saila@gmail.com>
2024-02-28 01:26:37 +0200

    Add odds calculation to main

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,11 @@ mod parse;
 mod calculate_odds;
 
 fn main() {
-    println!("Input dice as <amount>d<wanted value>");
+    println!("Input dice as <amount>x<die face amount>d<wanted value>");
     let stdin = io::stdin();
     let buffer = &mut String::new();
     stdin.read_line(buffer).expect("what?");
-    let _output = parse::parse_dice_str(buffer);
+    let roll = parse::parse_dice_str(buffer).unwrap();
+    let output = calculate_odds::calculate_roll_odds(&roll);
+    println!("{}", output);
 }
